ac60d1a48d5bee9042f5a6642019d9e7f64bc961
Dainius Jocas <dainius.jocas@tokenmill.lt>
2019-09-13 12:19:37 +0300

    feat: better graphs

--- README.md
+++ README.md
@@ -9,3 +9,5 @@ https://www.kaggle.com/snapcrack/all-the-news/downloads/all-the-news.zip/4
 ## Performance
 
 ![alt text](resources/average-per-doc.png)
+
+![alt text](resources/min-max-per-doc.png)
--- src/bench/phrases.clj
+++ src/bench/phrases.clj
@@ -41,18 +41,18 @@
       output)))
 
 (defn bench-one-cpu [dictionary articles]
-  (let [start (System/currentTimeMillis)
+  (let [start (System/nanoTime)
         annotator-fn (phrases/annotator dictionary)
-        setup-time (- (System/currentTimeMillis) start)
-        start (System/currentTimeMillis)
+        setup-time (- (System/nanoTime) start)
+        start (System/nanoTime)
         rez (doall
               (map (fn [article]
-                     (let [start (System/currentTimeMillis)]
+                     (let [start (System/nanoTime)]
                        (try
                          (let [annotations (annotator-fn (:content article))]
                            {:annotations annotations
                             :setup-time  setup-time
-                            :duration    (- (System/currentTimeMillis) start)})
+                            :duration    (- (System/nanoTime) start)})
                          (catch NullPointerException _
                            {:annotations []
                             :duration    -1})
@@ -61,46 +61,65 @@
                            {:annotations []
                             :duration    -1}))))
                    articles))
-        _ (log/infof "Annotated in %s ms" (- (System/currentTimeMillis) start))]
+        _ (log/infof "Annotated in %s ns" (- (System/nanoTime) start))]
     rez))
 
 (defn bench-one-cpu* [dictionary-file articles-file]
-  (let [dictionary (take 5000 (readers/read-csv dictionary-file))
-        articles (take 1000 (read-news-articles articles-file))]
+  (let [dictionary (take 50000 (readers/read-csv dictionary-file))
+        articles (take 5000 (read-news-articles articles-file))]
     (let [step (min 1000 (count dictionary))]
       (loop [cnt step
              result []]
         (if (<= cnt (count dictionary))
-          (let [start (System/currentTimeMillis)
+          (let [start (System/nanoTime)
                 rez (bench-one-cpu (take cnt dictionary) articles)
-                coll (filter pos-int? (map :duration rez))
+                succeeded (filter pos-int? (map :duration rez))
                 failed (filter neg-int? (map :duration rez))]
             (recur (+ cnt step)
                    (conj result {:dictionary-size (count (take cnt dictionary))
                                  :articles-count  (count articles)
                                  :failed          (count failed)
-                                 :total-time      (- (System/currentTimeMillis) start)
-                                 :min             (apply min coll)
-                                 :max             (apply max coll)
-                                 :average         (float (/ (reduce + coll) (count coll)))})))
+                                 :total-time      (- (System/nanoTime) start)
+                                 :min             (float (/ (apply min succeeded) 1000000))
+                                 :max             (float (/ (apply max succeeded) 1000000))
+                                 :average         (float (/ (reduce + succeeded) (count succeeded) 1000000))})))
           result)))))
 
 (defn preview-vals [vals]
   (oz/view!
     [:div
      [:h1 "Beagle performance"]
-     [:p "Average per doc and total time per doc"]
+     [:h2 "Average per doc and total time per doc in ms"]
      [:div {:style {:display "flex" :flex-direction "row"}}
       [:vega-lite {:data {:values vals}
+                   :width 400 :height 300
                    :encoding {:x {:field "dictionary-size"}
                               :y {:field "average"}
                               :color {:field "articles-count" :type "nominal"}}
-                   :mark "line"}]
+                   :mark "line"}]]
+     [:div {:style {:display "flex" :flex-direction "row"}}
       [:vega-lite {:data {:values vals}
+                   :width 400 :height 300
                    :encoding {:x {:field "dictionary-size"}
                               :y {:field "total-time"}
                               :color {:field "articles-count" :type "nominal"}}
                    :mark "line"}]]
+     [:div
+      [:h2 (format "MIN and MAX time spent per article for %s articles" (:articles-count (first vals)))]
+      [:vega-lite {:data     {:values vals}
+                   :width    400 :height 300
+                   "resolve" {"scale" {"y" "independent"}}
+                   :layer    [{:encoding {:x {:field "dictionary-size"
+                                              :axis   {"title" "Dictionary size"}}
+                                          :y {:field "max"
+                                              :axis   {"title"      "MAX time spent on one article in ms"
+                                                       "titleColor" "red"}}}
+                               :mark     {:type "line" :color "red"}}
+                              {:encoding {:x     {:field "dictionary-size"}
+                                          :y     {:field "min"
+                                                  :axis   {"title"      "MIN time spent on one article in ms"
+                                                           "titleColor" "green"}}}
+                               :mark     {:type "line" :color "green"}}]}]]
      [:h2 "Summary"]
      [:p "This sums up the performance benchmarks."]]))
 

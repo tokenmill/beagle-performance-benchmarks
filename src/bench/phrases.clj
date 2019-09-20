5d0279974f89ac1f7ec357f424b062372aae12e3
Dainius Jocas <dainius.jocas@tokenmill.lt>
2019-09-20 15:21:43 +0300

    refactor: separate benchmark and visualization

--- src/bench/phrases.clj
+++ src/bench/phrases.clj
@@ -4,7 +4,6 @@
             [clojure.tools.logging :as log]
             [clojure.data.csv :as csv]
             [clojure.core.async :refer [chan pipeline-blocking to-chan <!! close!]]
-            [oz.core :as oz]
             [beagle.readers :as readers]
             [beagle.phrases :as phrases]
             [jsonista.core :as json]))
@@ -61,56 +60,6 @@
         _ (log/infof "Annotated in %s ns" (- (System/nanoTime) start))]
     rez))
 
-(defn spec [{benchmark-data :data :as benchmark}]
-  [:div
-   [:h1 "Beagle performance"]
-   [:h2 "Times"]
-   [:div
-    [:h3 "Average time per doc in ms"]
-    [:vega-lite {:data     {:values benchmark-data}
-                 :width    400 :height 300
-                 :encoding {:x     {:field "dictionary-size"}
-                            :y     {:field "average"}
-                            :color {:field "articles-count" :type "nominal"}}
-                 :mark     "line"}]]
-   [:div
-    [:h3 (format "Total time per %s documents in ms" (:articles-count (first benchmark-data)))]
-    [:vega-lite {:data     {:values benchmark-data}
-                 :width    400 :height 300
-                 :encoding {:x     {:field "dictionary-size"}
-                            :y     {:field "total-time"}
-                            :color {:field "articles-count" :type "nominal"}}
-                 :mark     "line"}]]
-   [:div
-    [:h3 (format "Docs per second")]
-    [:vega-lite {:data     {:values benchmark-data}
-                 :width    400 :height 300
-                 :encoding {:x     {:field "dictionary-size"}
-                            :y     {:field "per-second"}
-                            :color {:field "articles-count" :type "nominal"}}
-                 :mark     "line"}]]
-   [:div
-    [:h2 (format "MIN and MAX time spent per article for %s articles" (:articles-count (first benchmark-data)))]
-    [:vega-lite {:data     {:values benchmark-data}
-                 :width    400 :height 300
-                 "resolve" {"scale" {"y" "independent"}}
-                 :layer    [{:encoding {:x {:field "dictionary-size"
-                                            :axis  {"title" "Dictionary size"}}
-                                        :y {:field "max"
-                                            :axis  {"title"      "MAX time spent on one article in ms"
-                                                    "titleColor" "red"}}}
-                             :mark     {:type "line" :color "red"}}
-                            {:encoding {:x {:field "dictionary-size"}
-                                        :y {:field "min"
-                                            :axis  {"title"      "MIN time spent on one article in ms"
-                                                    "titleColor" "green"}}}
-                             :mark     {:type "line" :color "green"}}]}]]
-   [:h2 "Summary"]
-   [:p "This sums up the performance benchmarks."]])
-
-(defn preview-vals [benchmark-data]
-  (oz/view! (spec benchmark-data)))
-
 (defn bench* [{:keys [bench-fn dictionary-file articles-file dictionary-step dictionary-entry-opts]}]
   (let [dictionary (map #(merge % dictionary-entry-opts) (readers/read-csv dictionary-file))
         articles (take 10000 (read-news-articles articles-file))]
@@ -150,5 +99,4 @@
                                                                         :ascii-fold?     true}
                                                 :dictionary-file       dictionary-file
                                                 :articles-file         articles-file})}]
-    (spit (str "vals-" (System/currentTimeMillis) ".json") (json/write-value-as-string benchmark))
-    (preview-vals benchmark)))
+    (spit (str "vals-" (System/currentTimeMillis) ".json") (json/write-value-as-string benchmark))))

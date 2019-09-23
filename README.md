6ca55e268dc08cf0d120e867853960c8dd73d1f1
Dainius Jocas <dainius@tokenmill.com>
2019-09-23 16:29:28 +0300

    feat: cli interface

--- README.md
+++ README.md
@@ -6,6 +6,35 @@
 
 Performance benchmarks for Beagle library and a comparison with other stored query solutions.
 
+## Benchmarking
+
+```bash
+clj -m bench.phrases
+```
+
+To see the available options run
+
+```bash
+clj -m bench.phrases -h
+```
+
+It outputs something like this:
+```
+  -d, --dictionary DICTIONARY          resources/top-10000.csv  Path to the dictionary file
+  -t, --texts TEXTS                    resources/articles1.csv  Path to the CSV file with texts
+  -s, --step STEP                      5000                     Step size for increase in dictionary
+  -p, --parallel PARALLEL              true                     Should the benchmark be run in parallel
+  -k, --key KEY                        :content                 CSV header key to select
+  -w, --warm-up WARM-UP                true                     Should the warm-up be run
+      --slop SLOP                      0                        Phrase slop for dictionary entries
+      --case-sensitive CASE_SENSITIVE  true                     Should matching be case sensitive
+      --ascii-fold ASCII_FOLD          false                    Should matching be ascii folded
+      --stem STEM                      false                    Should matching be stemmed
+      --stemmer STEMMER                :english                 which stemmer should be used
+  -h, --help
+```
+
+
 ## Download news dataset from
 
 https://www.kaggle.com/snapcrack/all-the-news/downloads/all-the-news.zip/4
--- src/bench/phrases.clj
+++ src/bench/phrases.clj
@@ -1,12 +1,13 @@
 (ns bench.phrases
   (:require [clojure.string :as s]
             [clojure.java.io :as io]
+            [clojure.tools.cli :as cli]
             [clojure.tools.logging :as log]
             [clojure.data.csv :as csv]
             [clojure.core.async :refer [chan pipeline-blocking to-chan <!! close!]]
+            [jsonista.core :as json]
             [beagle.readers :as readers]
-            [beagle.phrases :as phrases]
-            [jsonista.core :as json]))
+            [beagle.phrases :as phrases]))
 
 (defn read-news-articles [source]
   (with-open [reader (io/reader source)]
@@ -16,10 +17,10 @@
                                               attr-name))) header)]
       (doall (map #(apply hash-map (interleave kvs %)) lines)))))
 
-(defn annotate [annotator-fn article]
+(defn annotate [annotator-fn article key]
   (let [start (System/nanoTime)]
     (try
-      (let [annotations (annotator-fn (:content article))]
+      (let [annotations (annotator-fn (get article key))]
         {:annotations annotations
          :duration    (- (System/nanoTime) start)})
       (catch NullPointerException _
@@ -31,11 +32,12 @@
          :duration    -1}))))
 
 (defn bench-concurrent*
-  ([dictionary articles] (bench-concurrent* dictionary articles (* 2 (.availableProcessors (Runtime/getRuntime)))))
-  ([dictionary articles parallelism]
+  ([dictionary articles key]
+   (bench-concurrent* dictionary articles key (* 2 (.availableProcessors (Runtime/getRuntime)))))
+  ([dictionary articles key parallelism]
    (let [annotator-fn (phrases/annotator dictionary)
          out (chan (or parallelism 16))
-         xf (map #(annotate annotator-fn %))
+         xf (map #(annotate annotator-fn % key))
          start (System/nanoTime)]
      (pipeline-blocking
        (or parallelism 16)
@@ -47,28 +49,25 @@
        (log/infof "Annotated in %s ns" (- (System/nanoTime) start))
        output))))
 
-(defn multithreaded [dictionary-file articles-file parallelism]
-  (let [dictionary (readers/read-csv dictionary-file)
-        articles (read-news-articles articles-file)]
-    (log/infof "Dictionary size: %s; articles: %s" (count dictionary) (count articles))
-    (bench-concurrent* dictionary articles parallelism)))
-
-(defn bench-one-thread [dictionary articles]
+(defn bench-one-thread [dictionary articles key]
   (let [annotator-fn (phrases/annotator dictionary)
         start (System/nanoTime)
-        rez (doall (map #(annotate annotator-fn %) articles))
+        rez (doall (map #(annotate annotator-fn % key) articles))
         _ (log/infof "Annotated in %s ns" (- (System/nanoTime) start))]
     rez))
 
-(defn bench* [{:keys [bench-fn dictionary-file articles-file dictionary-step dictionary-entry-opts]}]
+(defn bench* [{:keys [warm-up bench-fn dictionary-file dictionary-step dictionary-entry-opts articles-file key]}]
   (let [dictionary (map #(merge % dictionary-entry-opts) (readers/read-csv dictionary-file))
-        articles (take 10000 (read-news-articles articles-file))]
+        articles (read-news-articles articles-file)]
     (let [step (min (or dictionary-step 5000) (count dictionary))]
+      (when warm-up
+        (log/infof "Doing a warm-up run.")
+        (bench-fn dictionary articles key))
       (loop [cnt step
              result []]
         (if (<= cnt (count dictionary))
           (let [start (System/nanoTime)
-                rez (bench-fn (take cnt dictionary) articles)
+                rez (bench-fn (take cnt dictionary) articles key)
                 succeeded (filter pos-int? (map :duration rez))
                 failed (filter neg-int? (map :duration rez))]
             (recur (+ cnt step)
@@ -82,21 +81,73 @@
                                  :average         (float (/ (reduce + succeeded) (count succeeded) 1000000))})))
           result)))))
 
-(defn -main [& {:as opts}]
-  (let [dictionary-file "resources/top-10000.csv"
-        articles-file "resources/articles1.csv"
-        bench-fn (if (:single-thread opts) bench-one-thread bench-concurrent*)
-        benchmark {:meta {:multi-thread (true? (:single-thread opts))
-                          :opts         opts
-                          :system       (System/getProperties)
-                          :runtime      {:total-memory (.totalMemory (Runtime/getRuntime))
-                                         :cpu          (.availableProcessors (Runtime/getRuntime))}}
-                   :data (bench.phrases/bench* {:bench-fn              bench-fn
-                                                :dictionary-step       5000
-                                                :dictionary-entry-opts {:slop            1
-                                                                        :case-sensitive? false
-                                                                        :stem?           true
-                                                                        :ascii-fold?     true}
-                                                :dictionary-file       dictionary-file
-                                                :articles-file         articles-file})}]
+(defn bench [{:keys [warm-up dictionary texts step parallel key slop case-sensitive stem ascii-fold stemmer] :as opts}]
+  (log/infof "Started benchmark with opts: '%s'" opts)
+  (let [bench-fn (if parallel bench-concurrent* bench-one-thread)
+        benchmark {:meta {:opts    opts
+                          :system  (System/getProperties)
+                          :runtime {:total-memory (.totalMemory (Runtime/getRuntime))
+                                    :cpu          (.availableProcessors (Runtime/getRuntime))}}
+                   :data (bench.phrases/bench* {:warm-up               warm-up
+                                                :bench-fn              bench-fn
+                                                :dictionary-step       step
+                                                :dictionary-entry-opts {:slop            slop
+                                                                        :case-sensitive? case-sensitive
+                                                                        :ascii-fold?     ascii-fold
+                                                                        :stem?           stem
+                                                                        :stemmer         stemmer}
+                                                :dictionary-file       dictionary
+                                                :articles-file         texts
+                                                :key                   key})}]
     (spit (str "vals-" (System/currentTimeMillis) ".json") (json/write-value-as-string benchmark))))
+
+(def cli-options
+  [["-d" "--dictionary DICTIONARY" "Path to the dictionary file"
+    :default "resources/top-10000.csv"]
+   ["-t" "--texts" "Path to the CSV file with texts"
+    :default "resources/articles1.csv"
+    :required "TEXTS"
+    :parse-fn #(str %)
+    :strict true]
+   ["-s" "--step STEP" "Step size for increase in dictionary"
+    :parse-fn #(Integer/parseInt %)
+    :default 5000]
+   ["-p" "--parallel PARALLEL" "Should the benchmark be run in parallel"
+    :parse-fn #(Boolean/parseBoolean %)
+    :default true]
+   ["-k" "--key KEY" "CSV header key to select"
+    :parse-fn #(keyword %)
+    :default :content]
+   ["-w" "--warm-up WARM-UP" "Should the warm-up be run"
+    :parse-fn #(Boolean/parseBoolean %)
+    :default true]
+   [nil "--slop SLOP" "Phrase slop for dictionary entries"
+    :parse-fn #(Integer/parseInt %)
+    :default 0]
+   [nil "--case-sensitive CASE_SENSITIVE" "Should matching be case sensitive"
+    :parse-fn #(Boolean/parseBoolean %)
+    :default true]
+   [nil "--ascii-fold ASCII_FOLD" "Should matching be ascii folded"
+    :parse-fn #(Boolean/parseBoolean %)
+    :default false]
+   [nil "--stem STEM" "Should matching be stemmed"
+    :parse-fn #(Boolean/parseBoolean %)
+    :default false]
+   [nil "--stemmer STEMMER" "which stemmer should be used"
+    :parse-fn #(keyword %)
+    :default :english]
+   ;; A boolean option defaulting to nil
+   ["-h" "--help"]])
+
+(defn -main [& args]
+  (let [{:keys [options summary] :as opts}
+        (cli/parse-opts args cli-options)]
+    (clojure.pprint/pprint opts)
+    (if (:help options)
+      (do
+        (println summary)
+        (System/exit 0))
+      (do
+        (bench options)
+        (shutdown-agents)
+        (System/exit 0)))))
